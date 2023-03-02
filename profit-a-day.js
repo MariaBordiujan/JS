dd6ab1845633d7d0a341334285c62e3016cd7db6
MariaBordiujan <118996724+MariaBordiujan@users.noreply.github.com>
2023-03-02 11:03:04 +0200

    Update profit-a-day.js

--- profit-a-day.js
+++ profit-a-day.js
@@ -2,10 +2,10 @@
 // Const - задает неизменяемую переменную, в отличие от let. 
 // Такие переменные называются константами. 
 
-
+// HW2
 let paymentPerHour = 10
 let workingHours = prompt("How many hours a day do you work?")
 
 let paymentPerDay = paymentPerHour * workingHours
 
-alert (paymentPerDay)
+alert (`Your payment per day is ${paymentPerDay}$`)
