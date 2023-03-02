29df50a47749f097608dcc1d707f73dc0bdef65c
MariaBordiujan <118996724+MariaBordiujan@users.noreply.github.com>
2023-03-02 10:58:16 +0200

    Add files via upload

---
+++ profit-a-day.js
@@ -0,0 +1,11 @@
+// HW1
+// Const - задает неизменяемую переменную, в отличие от let. 
+// Такие переменные называются константами. 
+
+
+let paymentPerHour = 10
+let workingHours = prompt("How many hours a day do you work?")
+
+let paymentPerDay = paymentPerHour * workingHours
+
+alert (paymentPerDay)
