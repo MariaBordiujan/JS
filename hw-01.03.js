98165de7904d8e94b11864dce8de05605c82a7af
MariaBordiujan <118996724+MariaBordiujan@users.noreply.github.com>
2023-03-06 17:06:45 +0200

    Update hw-01.03.js

--- hw-01.03.js
+++ hw-01.03.js
@@ -1,10 +1,13 @@
 a = Number(prompt("Enter \"A\" value"))
 b = Number(prompt("Enter \"B\" value"))
 
-
-alert(`a = ${a}
-b = ${b}\n
+alert(
+`######## VALUES ####### 
+a = ${a}
+b = ${b}
+###### OPERATIONS #####
 a + b = ${a+b}
 a - b = ${a-b}
 a * b = ${a*b}
-a / b = ${a/b}`)
+a / b = ${a/b}
+#######################`)
