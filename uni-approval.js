3a64527c8cc0af4f98927a04dc632a09a4cdce25
MariaBordiujan <118996724+MariaBordiujan@users.noreply.github.com>
2023-03-09 14:48:21 +0200

    Add files via upload

---
+++ uni-approval.js
@@ -0,0 +1,8 @@
+let studentGeneralMark   = 10.0
+let studentBudget        = 50_000
+let isConnected          = true
+
+const universityGrade    = 7.0
+const universityContract = 40_000
+
+let approve = studentGeneralMark >= universityGrade || studentBudget >= universityContract || isConnected
